f562a93bdeb995689db56b4bcdf07b7104ba44ab
Stavya3 <Stavyakc03@gmail.com>
2024-11-15 06:46:33 +0530

    Done with blogs content page

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 
 
 interface BlogCardProps {
@@ -14,28 +14,54 @@ const BlogCard = ({
     publishedDate
 }: BlogCardProps) => {
   return (
-    <div>
-        <div>
-            {authorName} . {publishedDate}
+    <div className=" p-4 border-b border-slate-300">
+        <div className="flex">
+            <div className="flex justify-center flex-col">
+                <Avatar name={authorName} /> 
+            </div>
+            
+            <div className="font-extralight pl-2 text-sm flex justify-center flex-col">
+                {authorName}
+            </div> 
+            <div className="flex justify-center flex-col pl-2">
+                <Circle />
+            </div>
+            <div className="pl-2 font-thin text-slate-500 text-sm flex justify-center flex-col">{publishedDate}</div>
         </div>
 
-        <div>
+        <div className="text-black font-semibold text-xl pt-2">
             {title}
         </div>
 
-        <div>
+        <div className="text-slate-800 text-md font-thin">
             {content.slice(0, 100) + "..."}
         </div>
         
-        <div>
-            {`${Math.ceil(content.length / 100)} minutes`}
+        <div className="text-slate-500 text-sm font-thin pt-4">
+            {`${Math.ceil(content.length / 100)} minute(s) read`}
         </div>
 
-        <div className='bg-slate-200 h-1 w-full'>
-
-        </div>
+        
     </div>
   )
+}
+
+function Circle(){
+    return(
+        <div className="h-1 w-1 rounded-full bg-slate-500">
+
+        </div>
+    )
+}
+
+function Avatar({ name }: { name: string }){
+    return( 
+
+    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+        <span className="font-thin text-xs text-gray-600 dark:text-gray-300">{name[0]}</span>
+    </div>
+    )
+    
 }
 
 export default BlogCard
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -3,14 +3,29 @@ import BlogCard from '../components/BlogCard'
 
 const Blogs = () => {
   return (
-    <div>
-        <BlogCard
+    <div className='flex justify-center'>
+      <div className='max-w-xl'>
+          <BlogCard
             authorName={"Stavya"}
-            title={"Title of the blog"}
-            content={"Content of the blog"}
+            title={"How to make money while you sleep and be lazy the entire time"}
+            content={"How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time"}
             publishedDate={"2nd Feb 2024"}
-        />
+          />
+          <BlogCard
+            authorName={"Stavya"}
+            title={"How to make money while you sleep and be lazy the entire time"}
+            content={"How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time"}
+            publishedDate={"2nd Feb 2024"}
+          />
+          <BlogCard
+            authorName={"Stavya"}
+            title={"How to make money while you sleep and be lazy the entire time"}
+            content={"How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time How to make money while you sleep and be lazy the entire time"}
+            publishedDate={"2nd Feb 2024"}
+          />
+      </div>
     </div>
+    
   )
 }
 
